894616b8209297ac1a3d781a858cd46c02158f61
Roger Vargas <rogervargas@Rogers-MacBook-Pro.local>
2022-01-27 08:45:14 -0600

    fixed import of functions in init file by add . precursor

--- BioPlexPy/__init__.py
+++ BioPlexPy/__init__.py
@@ -1,3 +1,3 @@
-from helper_funcs import getBioPlex
-from helper_funcs import getGSE122425
-from helper_funcs import bioplex2graph
+from .helper_funcs import getBioPlex
+from .helper_funcs import getGSE122425
+from .helper_funcs import bioplex2graph
