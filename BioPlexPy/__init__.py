7d4a45de911c348e1f91163d49618f3e7fad5883
Roger Vargas <rv76@login05.o2.rc.hms.harvard.edu>
2022-02-22 15:11:22 -0500

    add functionality to visualize PPI network for a CORUM complex

--- BioPlexPy/__init__.py
+++ BioPlexPy/__init__.py
@@ -2,3 +2,4 @@ from .helper_funcs import getBioPlex
 from .helper_funcs import getGSE122425
 from .helper_funcs import bioplex2graph
 from .helper_funcs import getCorum
+from .helper_funcs import display_PPI_network_for_complex
--- BioPlexPy/helper_funcs.py
+++ BioPlexPy/helper_funcs.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
+
 import io
 import requests
+import itertools
 import anndata as ad
 import pandas as pd
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
 
 def getBioPlex(cell_line, version):
     '''
     Load BioPlex interactions data.
     
     This function loads BioPlex PPI data for
-    for cell lines HEK293T and HCT116, note we
+    cell lines HEK293T and HCT116, note we
     only have version 1.0 for HCT116 cells.
     
     Parameters
@@ -100,7 +105,7 @@ def bioplex2graph(bp_PPI_df):
     Convert BioPlex PPIs into a graph.
     
     This function converts representation of BioPlex PPIs into a graph data structure
-    Representation of BioPlex PPIs in a NetworkX object from NetworkX. 
+    representation of BioPlex PPIs in a NetworkX object from NetworkX. 
 
     Parameters
     ----------
@@ -183,7 +188,7 @@ def getCorum(complex_set = 'all', organism = 'Human'):
     Parameters
     ----------
     complex_set : str
-        Takes input [all','core','splice'] (default 'all').
+        Takes input ['all','core','splice'] (default 'all').
     organism : str
         Takes input ['Bovine','Dog','Hamster','Human','MINK','Mammalia','Mouse','Pig','Rabbit','Rat'] (default 'Human').
 
@@ -212,3 +217,109 @@ def getCorum(complex_set = 'all', organism = 'Human'):
     CORUM_df.reset_index(inplace = True, drop = True)
     
     return CORUM_df
+
+def display_PPI_network_for_complex(bp_PPI_df, Corum_DF, ComplexName_i, node_size, edge_width, fig_out_path):
+    '''
+    Display network of BioPlex PPIs for a CORUM complex.
+    
+    This function displays a complete network in which nodes represent 
+    the proteins in a specified CORUM complex and edges represent 
+    BioPlex PPIs using NetworkX. Edges detected through AP-MS are colored darker.
+
+    Parameters
+    ----------
+    DataFrame of PPIs : Pandas DataFrame
+    DataFrame of CORUM complexes : Pandas DataFrame
+    Name of Corum Complex: str
+    Size of Nodes in Network: int
+    Width of Edges in Network: float
+    Path to save figure: str
+
+    Returns
+    -------
+    PNG file
+        Displays and outputs a PNG file of network.
+
+    Examples
+    --------
+    >>> bp_293t_df = getBioPlex('293T', '3.0') # (1) Obtain the latest version of the 293T PPI network
+    >>> Corum_DF = getCorum('core', 'Human') # (2) Obtain CORUM complexes
+    >>> display_PPI_network_for_complex(bp_PPI_df, Corum_DF, 'ING2 complex', 2300, 3.5, /n/shared_db/ccb/bioplex/BioPlexPy_testing/figures/network_293T_3.0_ING2-complex.png') # (3) Visualize network for specified protein complex using PPI data
+    '''
+    # store gene symbols that belong to this complex in a list
+    genes_in_complex_i = Corum_DF[Corum_DF.ComplexName == ComplexName_i].loc[:,'subunits(Gene name)'].values[0].split(';')
+
+    # filter BioPlex PPI dataframe to include only interactions where both genes are found in complex
+    complex_i_PPI_filter = []
+    for symbol_A, symbol_B in zip(bp_PPI_df.SymbolA, bp_PPI_df.SymbolB):
+
+        # check to see if both gene symbols for this interaction are genes in complex
+        if (symbol_A in genes_in_complex_i) and (symbol_B in genes_in_complex_i):
+            complex_i_PPI_filter.append(True)
+        else:
+            complex_i_PPI_filter.append(False)
+
+    complex_i_PPI_filter = np.array(complex_i_PPI_filter)
+    bp_complex_i_df = bp_PPI_df[complex_i_PPI_filter] # use filter to subset bp PPI dataframe
+    bp_complex_i_df.reset_index(inplace = True, drop = True) # reset index
+    bp_complex_i_G = bioplex2graph(bp_complex_i_df) # turn subsetted bp PPI df into a graph
+    bp_complex_i_G = bp_complex_i_G.to_undirected() # convert to undirected graph for displaying
+
+    # get a list of genes that were identifed as "baits" and "preys" for coloring nodes
+    bp_complex_i_baits = list(set(bp_complex_i_df.SymbolA))
+    bp_complex_i_preys = list(set(bp_complex_i_df.SymbolB))
+
+    # labels will be the gene symbols, pull from attribute for each node
+    labels = nx.get_node_attributes(bp_complex_i_G, 'symbol')
+
+    # color nodes according to whether they were present among "baits" & "preys", just "baits", or just "preys" for this complex
+    node_color_map = []
+    for node_i_uniprot in bp_complex_i_G.nodes:
+
+        node_i_gene_symbol = labels[node_i_uniprot] # get gene symbol
+
+        # gene is present among baits & preys for the PPIs detected in this complex
+        if (node_i_gene_symbol in bp_complex_i_baits) and (node_i_gene_symbol in bp_complex_i_preys):
+            node_color_map.append('xkcd:red')
+
+        # gene is present among baits but NOT preys for the PPIs detected in this complex
+        elif (node_i_gene_symbol in bp_complex_i_baits) and (node_i_gene_symbol not in bp_complex_i_preys):
+            node_color_map.append('xkcd:red')
+
+        # gene is NOT present among baits & but is present among preys for the PPIs detected in this complex
+        elif (node_i_gene_symbol not in bp_complex_i_baits) and (node_i_gene_symbol in bp_complex_i_preys):
+            node_color_map.append('xkcd:rose pink')
+
+    # create a complete graph from the nodes of complex graph to add in all "background" edges (edges detected with AP-MS will be colored over)
+    # position will be the same for both graphs since nodes are the same
+    bp_complex_i_G_complete = nx.Graph()
+    bp_complex_i_G_complete.add_nodes_from(bp_complex_i_G.nodes)
+    bp_complex_i_G_complete.add_edges_from(itertools.combinations(bp_complex_i_G.nodes, 2))
+
+    # create figure instance
+    fig, ax = plt.subplots()
+
+    # set position of nodes
+    pos = nx.circular_layout(bp_complex_i_G)
+
+    # construct edges for COMPLETE graph for "background" edges
+    edges_complete = nx.draw_networkx_edges(bp_complex_i_G_complete, pos, width = edge_width, alpha = 0.25)
+    edges_complete.set_edgecolor("xkcd:grey")
+
+    # construct edges
+    edges = nx.draw_networkx_edges(bp_complex_i_G, pos, width = edge_width)
+    edges.set_edgecolor("xkcd:red")
+
+    # construct nodes
+    nodes = nx.draw_networkx_nodes(bp_complex_i_G, pos, node_size = node_size, node_color = node_color_map)
+    nodes.set_edgecolor("xkcd:black")
+    nodes.set_linewidth(1.5)
+    nx.draw_networkx_labels(bp_complex_i_G, pos, labels = labels, font_size = 10, font_weight = 'bold', font_color = 'xkcd:white')
+
+    fig = plt.gcf()
+    fig.set_size_inches(7.5, 7.5)
+    fig.tight_layout()
+    
+    # save figure as PNG
+    plt.savefig(fig_out_path, bbox_inches='tight', dpi = 300 , transparent = True)
+    plt.show()
