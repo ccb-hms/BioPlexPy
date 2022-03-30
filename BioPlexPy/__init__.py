f928f582b0f61c4f56770450424cd0dbce336319
Roger Vargas <rv76@compute-a-16-161.o2.rc.hms.harvard.edu>
2022-03-30 12:41:23 -0400

    changed function name and added a function to analysis funcs

--- BioPlexPy/__init__.py
+++ BioPlexPy/__init__.py
@@ -2,7 +2,8 @@ from .data_import_funcs import getBioPlex
 from .data_import_funcs import getGSE122425
 from .data_import_funcs import getCorum
 from .analysis_funcs import bioplex2graph
-from .analysis_funcs import get_PPI_network_edges_for_complex
+from .analysis_funcs import get_PPI_network_for_complex
+from .analysis_funcs import get_DataFrame_from_PPI_network
 from .analysis_funcs import get_prop_edges_in_complex_identfied
 from .analysis_funcs import permutation_test_for_CORUM_complex
 from .visualization_funcs import display_PPI_network_for_complex
