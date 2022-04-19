f4be4ea56bb5e378478fce5d2382098adafd2f0f
Roger Vargas <rv76@compute-e-16-237.o2.rc.hms.harvard.edu>
2022-04-19 18:17:11 -0400

    changed func name get_prop_edges_in_complex_identified

--- BioPlexPy/__init__.py
+++ BioPlexPy/__init__.py
@@ -4,6 +4,6 @@ from .data_import_funcs import getCorum
 from .analysis_funcs import bioplex2graph
 from .analysis_funcs import get_PPI_network_for_complex
 from .analysis_funcs import get_DataFrame_from_PPI_network
-from .analysis_funcs import get_prop_edges_in_complex_identfied
+from .analysis_funcs import get_prop_edges_in_complex_identified
 from .analysis_funcs import permutation_test_for_CORUM_complex
 from .visualization_funcs import display_PPI_network_for_complex
